313310b8578c173538c4377e7db94875222ab334
isaaclyra <isaac.lyra.junior@gmail.com>
2022-11-07 04:30:53 -0300

    interface de autenticação, contexto de autenticação, autenticação com google(OAuth)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,18 +5,21 @@ import { SignIn } from './src/screens/SignIn';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme'
+import { AuthContextProvider } from "./src/contexts/AuthContext";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <AuthContextProvider>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
 
-      { fontsLoaded ? <SignIn />: <Loading /> }
+        { fontsLoaded ? <SignIn />: <Loading /> }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
